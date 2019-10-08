e82bd609904b8c91bf7aa4f52e410a963fa87c37
Zach Kaylor <avanasear@gmail.com>
2019-10-07 21:53:29 -0400

    added some useless shit

--- src/main.c
+++ src/main.c
@@ -1,16 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/sysinfo.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
 #include <gcrypt.h>
 #include <math.h>
 
+#define NPROCS get_nprocs()
+
 int main(const int argc, const char ** argv){
 	/* Main function. Arg handling, function calling, etc. */
 
-	printf("Not implemented yet.\n");
-	printf("%d; %s\n", argc, argv[0]);
+	int i, pid;
+
+	for (i=0; i<NPROCS; i++){
+		pid = fork();
+		if (pid < 0){
+			printf("Error creating process.\n");
+			exit(1);
+		}
+		else if (pid == 0){
+			printf("proc %d::\npid: %d\nargc: %d\n", i, getpid(), argc);
+			int j = 0;
+			while (argv[j]){
+				printf("argv[%d]: %s\n", j, argv[j]);
+				j++;
+			}
+			printf("\n");
+		}
+		else {
+			wait(NULL);
+		}
+	}
 
 	return 0;
 }
