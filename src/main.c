017b9e8c40aacbb50a27237563f205cd11445718
Zach Kaylor <avanasear@gmail.com>
2019-10-07 21:38:18 -0400

    added some starting code that does literally nothing

---
+++ src/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <gcrypt.h>
+#include <math.h>
+
+int main(const int argc, const char ** argv){
+	/* Main function. Arg handling, function calling, etc. */
+
+	printf("Not implemented yet.\n");
+	printf("%d; %s\n", argc, argv[0]);
+
+	return 0;
+}
