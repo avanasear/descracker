68ec0dd45c96898bb12e931892c7df57a39a7e1a
Zach Kaylor <avanasear@gmail.com>
2019-10-08 22:24:51 -0400

    added some comments, removed an unused function

--- src/main.c
+++ src/main.c
@@ -12,17 +12,21 @@
    if we decide to make this public after we finish. I don't know SHIT
    about licensing, so I guess we'll need to figure that out later.    */
 
+// make sure we use every available processor
 #define NPROCS get_nprocs()
 
 struct thread_args {
+	// keyspace arguments to pass into a thread
 	long long unsigned start;
 	long long unsigned end;
 };
 
 long long unsigned * find_keyspaces(void){
-	long long unsigned * keyspace = malloc(sizeof(long long unsigned) * NPROCS);
+	/* Function to break up the available keyspace into NPROCS chunks */
+
 	int i;
 	long long unsigned max_key = pow(2, 56);
+	long long unsigned * keyspace = malloc(sizeof(long long unsigned) * NPROCS);
 
 	for (i=0; i<NPROCS; i++){
 		keyspace[i] = max_key / NPROCS * i;
@@ -31,16 +35,16 @@ long long unsigned * find_keyspaces(void){
 	return keyspace;
 }
 
-void * test_print(void * space){
-	struct thread_args * keyspace = space;
-	printf("%llu\n%llu\n", keyspace->start, keyspace->end);
-}
-
 void * print_thread(void * inputs){
+	/* Test function to see if threads are properly created.
+	   I'm not a great programmer; don't judge me. */
+
+	// Get our needed variables from the function inputs
 	struct thread_args * space = inputs;
 	long long unsigned start = space->start;
 	long long unsigned end = space->end;
 
+	// Print every key % a billion (save some cycles by using mod)
 	long long unsigned i;
 	for (i=start; i<end; i++){
 		if (i % 1000000000 == 0){
@@ -56,6 +60,8 @@ int main(const int argc, const char ** argv){
 	pthread_t threads[NPROCS];
 	long long unsigned * keyspace = find_keyspaces();
 
+	/*
+	// Print the keyspace for some of the threads
 	printf("Thread 0 using keyspace: %llu::%llu\n", keyspace[0], keyspace[1]-1);
 	printf("Thread 1 using keyspace: %llu::%llu\n", keyspace[1], keyspace[2]-1);
 	printf("Thread 2 using keyspace: %llu::%llu\n", keyspace[2], keyspace[3]-1);
@@ -63,8 +69,9 @@ int main(const int argc, const char ** argv){
 	printf("Thread 4 using keyspace: %llu::%llu\n", keyspace[4], keyspace[5]-1);
 	printf("Thread 5 using keyspace: %llu::%llu\n", keyspace[5], keyspace[6]-1);
 	printf("Thread 6 using keyspace: %llu::%llu\n", keyspace[6], keyspace[7]-1);
-	printf("Thread 7 using keyspace: %llu::72057594037927936\n\n", keyspace[7]);
+	printf("Thread 7 using keyspace: %llu::72057594037927936\n\n", keyspace[7]); */
 
+	// Create all our threads and give them their keyspaces.
 	struct thread_args *args;
 	for (i=0; i<NPROCS; i++){
 		args = malloc(sizeof(struct thread_args));
@@ -77,11 +84,14 @@ int main(const int argc, const char ** argv){
 			exit(1);
 		}
 	}
+
+	// Don't judge me.
 	for (i=0; i<NPROCS; i++){
 		pthread_join(threads[i], NULL);
 	}
 
 	free(keyspace);
+	free(args);
 
 	return 0;
 }
