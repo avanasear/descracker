0123358eb4c433ab7794618459210abed4c881e2
Zach Kaylor <avanasear@gmail.com>
2019-10-08 22:15:47 -0400

    should be a good test

--- src/main.c
+++ src/main.c
@@ -19,11 +19,6 @@ struct thread_args {
 	long long unsigned end;
 };
 
-void * test_print(void * space){
-	struct thread_args * keyspace = space;
-	printf("%llu\n%llu\n", keyspace->start, keyspace->end);
-}
-
 long long unsigned * find_keyspaces(void){
 	long long unsigned * keyspace = malloc(sizeof(long long unsigned) * NPROCS);
 	int i;
@@ -36,9 +31,28 @@ long long unsigned * find_keyspaces(void){
 	return keyspace;
 }
 
+void * test_print(void * space){
+	struct thread_args * keyspace = space;
+	printf("%llu\n%llu\n", keyspace->start, keyspace->end);
+}
+
+void * print_thread(void * inputs){
+	struct thread_args * space = inputs;
+	long long unsigned start = space->start;
+	long long unsigned end = space->end;
+
+	long long unsigned i;
+	for (i=start; i<end; i++){
+		if (i % 1000000000 == 0){
+			printf("%llu\n", i);
+		}
+	}
+}
+
 int main(const int argc, const char ** argv){
 	/* Main function. Arg handling, function calling, etc. */
 
+	int i, rc;
 	pthread_t threads[NPROCS];
 	long long unsigned * keyspace = find_keyspaces();
 
@@ -49,7 +63,23 @@ int main(const int argc, const char ** argv){
 	printf("Thread 4 using keyspace: %llu::%llu\n", keyspace[4], keyspace[5]-1);
 	printf("Thread 5 using keyspace: %llu::%llu\n", keyspace[5], keyspace[6]-1);
 	printf("Thread 6 using keyspace: %llu::%llu\n", keyspace[6], keyspace[7]-1);
-	printf("Thread 7 using keyspace: %llu::72057594037927936\n", keyspace[7]);
+	printf("Thread 7 using keyspace: %llu::72057594037927936\n\n", keyspace[7]);
+
+	struct thread_args *args;
+	for (i=0; i<NPROCS; i++){
+		args = malloc(sizeof(struct thread_args));
+		args->start = keyspace[i];
+		args->end = keyspace[i] + keyspace[1];
+		// keyspace[1] will always be the same size as one broken-up keyspace
+		rc = pthread_create(&threads[i], NULL, print_thread, args);
+		if (rc) {
+			printf("Error - pthread_create() returned %d\n", rc);
+			exit(1);
+		}
+	}
+	for (i=0; i<NPROCS; i++){
+		pthread_join(threads[i], NULL);
+	}
 
 	free(keyspace);
 
