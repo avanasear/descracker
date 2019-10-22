7aeff89d612488207fe3fd29527827f327dc0ac5
Zach Kaylor <avanasear@gmail.com>
2019-10-22 18:21:05 -0400

    working prototype

--- src/main.c
+++ src/main.c
@@ -7,23 +7,6 @@
 #include <openssl/des.h>
 #include <math.h>
 
-/* Gonna put some notes right here...
-
-   DES_ecb_encrypt() is the basic DES encryption routine that encrypts or
-   decrypts a single 8-byte DES_cblock in electronic code book (ECB) mode.
-   It always transforms the input data, pointed to by input, into the output
-   data, pointed to by the output argument. If the encrypt argument is
-   non-zero (DES_ENCRYPT), the input (cleartext) is encrypted in to the
-   output (ciphertext) using the key_schedule specified by the schedule
-   argument, previously set via DES_set_key. If encrypt is zero
-   (DES_DECRYPT), the input (now ciphertext) is decrypted into the output
-   (now cleartext). Input and output may overlap. DES_ecb_encrypt() does not
-   return a value. (https://www.openssl.org/docs/man1.0.2/man3/des.html)
-
-   That site includes more information about it - need to set key/schedule/etc
-
-*/
-
 // make sure we use every available processor
 #define NPROCS get_nprocs()
 
@@ -37,65 +20,14 @@ struct thread_args {
 	const_DES_cblock cipher;
 };
 
-long long unsigned * find_keyspaces(void){
-	/* Function to break up the available keyspace into NPROCS chunks */
-
-	int i;
-	long long unsigned max_key = pow(2, 56);
-	long long unsigned * keyspace = malloc(sizeof(long long unsigned) * NPROCS);
-
-	for (i=0; i<NPROCS; i++){
-		keyspace[i] = max_key / NPROCS * i;
-	}
-
-	return keyspace;
-}
-
-void llu_to_des_key(int llu_int, const_DES_cblock key){
-	// idk how to do this yet
-}
-
-void * decrypt_thread(void * inputs){
-	/* Test function to see if threads are properly created.
-	   I'm not a great programmer; don't judge me. */
-
-	// Get our needed variables from the function inputs
-	struct thread_args * space = inputs;
-	long long unsigned start = space->start;
-	long long unsigned end = space->end;
-	const_DES_cblock des_input[8];
-	memcpy(des_input, space->cipher, 8);
-	DES_cblock dec_out[sizeof(des_input)];
-	const_DES_cblock key_candidate = {0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00};
-
-	DES_key_schedule key_sched;
-
-	// Print every key % a billion (save some cycles by using mod)
-	long long unsigned i;
-	for (i=start; i<end; i++){
-		if (found_a_key == 0){
-			// attempt to decrypt
-			llu_to_des_key(i, key_candidate);
-			DES_set_key_unchecked(&key_candidate, &key_sched);
-			DES_ecb_encrypt(des_input, dec_out, &key_sched, DES_DECRYPT);
-
-			// if we found a key, tell all the other processes to stop
-			/*if (dec_out == plain_text){
-				found_a_key=1;
-				break;
-			}
-			else {
-				continue;
-			}*/
-		}
-		else {
-			break;
-		}
-	}
-}
+// initialize functions
+long long unsigned * find_keyspaces(void);
+void * decrypt_thread(void * inputs);
+void print_data(const void * data, int len);
+void print_plain(const void * data, int len);
 
 int main(const int argc, const char ** argv){
-	/* Main function. Arg handling, function calling, etc. */
+	// Main function. Arg handling, function calling, etc.
 
 	printf("%d; %s\n", argc, argv[0]); // just to remove 'unused variable's
 
@@ -106,12 +38,7 @@ int main(const int argc, const char ** argv){
 	// open "encrypted.txt" to read 8 bytes into our buffer
 	FILE * fptr = fopen("encrypted.txt", "r");
 	const_DES_cblock ciphertext[8];
-	for (i=0; i<8; i++){
-		if (!fscanf(fptr, "%c", ciphertext[i])){
-			printf("Error reading file.\n");
-			exit(1);
-		}
-	}
+	fread(ciphertext, sizeof(unsigned char), 8, fptr);
 
 	// Create all our threads and give them their keyspaces.
 	struct thread_args *args;
@@ -137,4 +64,100 @@ int main(const int argc, const char ** argv){
 	free(args);
 
 	return 0;
+}
+
+long long unsigned * find_keyspaces(void){
+	// Function to break up the available keyspace into NPROCS chunks.
+
+	int i;
+	long long unsigned max_key = pow(2, 56);
+	long long unsigned * keyspace = malloc(sizeof(long long unsigned) * NPROCS);
+
+	for (i=0; i<NPROCS; i++){
+		keyspace[i] = max_key / NPROCS * i;
+	}
+
+	return keyspace;
+}
+
+void * decrypt_thread(void * inputs){
+	// Attempt to decrypt the ciphertext.
+
+	// Get our needed variables from the function inputs
+	struct thread_args * space = inputs;
+	long long unsigned start = space->start;
+	long long unsigned end = space->end;
+	const_DES_cblock des_input[8];
+	memcpy(des_input, space->cipher, 8);
+
+	DES_cblock dec_out[8];
+	DES_key_schedule key_sched;
+
+	// Print every key % a billion (save some cycles by using mod)
+	long long unsigned i;
+	for (i=start; i<end; i++){
+		if (found_a_key == 0){
+			// create key candidate to try
+			const_DES_cblock key_candidate = {
+				(unsigned char)(((i >> 49)       ) | 0x00),
+				(unsigned char)(((i >> 42) & 0x7f) | 0x00),
+				(unsigned char)(((i >> 35) & 0x7f) | 0x00),
+				(unsigned char)(((i >> 28) & 0x7f) | 0x00),
+				(unsigned char)(((i >> 21) & 0x7f) | 0x00),
+				(unsigned char)(((i >> 14) & 0x7f) | 0x00),
+				(unsigned char)(((i >>  7) & 0x7f) | 0x00),
+				(unsigned char)(((i)       & 0x7f) | 0x00)
+			};
+
+			// set keyschedule and decrypt
+			DES_set_key_unchecked(&key_candidate, &key_sched);
+			DES_ecb_encrypt(des_input, dec_out, &key_sched, DES_DECRYPT);
+
+			// if we found a key, tell all the other processes to stop
+			if (memcmp(dec_out, plaintext, 8) == 0){
+				found_a_key = 1;
+				printf("Found a working key: ");
+				print_data(key_candidate, 8);
+				printf("Plaintext bytes: ");
+				print_data(dec_out, 8);
+				printf("Plaintext: ");
+				print_plain(dec_out, 8);
+				break;
+			}
+			else {
+				continue;
+			}
+		}
+		else {
+			break;
+		}
+	}
+}
+
+void print_data(const void * data, int len){
+	/* Function to print (usually 8) bytes so that I don't have to make a 
+	   for loop every time */
+
+	int i;
+	const unsigned char * p = (const unsigned char *) data;
+
+	// print the array
+	for (i=0; i<len; ++i){
+		printf("%02x ", *p++);
+	}
+	printf("\n");
+}
+
+void print_plain(const void * data, int len){
+	/* Function to print (usually 8) bytes so that I don't have to make a 
+	   for loop every time */
+
+	int i;
+	const unsigned char * p = (const unsigned char *) data;
+
+	// print the array
+	for (i=0; i<len; ++i){
+		printf("%c", *p++);
+	}
+	printf("\n");
 }
