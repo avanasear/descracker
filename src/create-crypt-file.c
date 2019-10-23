d4528eb9e01478c730724ecec5b1eff330ea527e
Zach Kaylor <avanasear@gmail.com>
2019-10-23 16:09:44 -0400

    Allowed for entering custom plaintext by taking input from stdin.

--- src/create-crypt-file.c
+++ src/create-crypt-file.c
@@ -8,14 +8,27 @@ const_DES_cblock des_key = {0x00,0x00,0x00,0x00,0x32,0x20,0x20,0x20};
 
 void print_data(const void * data, int len);
 
-int main(){
+int main(void){
+	char plaintext[9] = {0};
+	printf("Enter 8 characters of plaintext: ");
+	fgets(plaintext, 9, stdin);
+	printf("Encrypting '%s'...\n", plaintext);
+
 	// 64-bit DES input data
-	const_DES_cblock des_input = {0x6c,0x6f,0x6c,0x6c,0x6f,0x73,0x65,0x72};
+	const_DES_cblock des_input = {
+		plaintext[0],
+		plaintext[1],
+		plaintext[2],
+		plaintext[3],
+		plaintext[4],
+		plaintext[5],
+		plaintext[6],
+		plaintext[7]
+	};
 	// Don't need an IV if we're using ECB.
 
 	// Initialize some buffers to hold our encrypted data and decrypted data
 	DES_cblock enc_out[sizeof(des_input)];
-	DES_cblock dec_out[sizeof(des_input)];
 
 	// Create DES key schedule
 	DES_key_schedule key_sched;
@@ -23,18 +36,19 @@ int main(){
 
 	// Encrypt our input and decrypt our encrypted output
 	DES_ecb_encrypt(&des_input, enc_out, &key_sched, DES_ENCRYPT);
-	DES_ecb_encrypt(enc_out, dec_out, &key_sched, DES_DECRYPT);
 
 	// Print our stuff out
-	printf("Input: \n");
+	printf("Unencrypted bytes: \n");
 	print_data(des_input, 8);
-	printf("Encrypted: \n");
+	printf("Encrypted bytes: \n");
 	print_data(enc_out, 8);
-	printf("Decrypted: \n");
-	print_data(dec_out, 8);
 
-	printf("Writing our encrypted data to the `encrypted.txt` file...\n");
-	FILE * fptr = fopen("encrypted.txt", "w");
+	printf("Writing our unencrypted data to the `plaintext.txt` file...\n");
+	FILE * fptr = fopen("plaintext.txt", "w");
+	fwrite(&des_input, 1, 8, fptr);
+	fclose(fptr);
+	printf("Writing our encrypted data to the `ciphertext.txt` file...\n");
+	fptr = fopen("ciphertext.txt", "w");
 	fwrite(&enc_out, 1, 8, fptr);
 	fclose(fptr);
 }
